32d92ff1050a7760ba08d8c5e7842c9b48a633cd
Mariusz Smajdor <smaidorr@gmail.com>
2020-10-14 18:54:09 +0200

    Updates

---
+++ precache-manifest.14efb05faeb5bdf0718e39ca89f354fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586ad74789d9f16336e2b3535ffb4181",
+    "url": "/to-do-list-react/index.html"
+  },
+  {
+    "revision": "19db747b77dce5d8231e",
+    "url": "/to-do-list-react/static/css/main.5566a0f3.chunk.css"
+  },
+  {
+    "revision": "56ab7f46fb79e61aaa1e",
+    "url": "/to-do-list-react/static/js/2.ccf67aed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list-react/static/js/2.ccf67aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19db747b77dce5d8231e",
+    "url": "/to-do-list-react/static/js/main.9aa0a3ff.chunk.js"
+  },
+  {
+    "revision": "4f831eb39a375a9667d0",
+    "url": "/to-do-list-react/static/js/runtime-main.55ffe52c.js"
+  }
+]);
