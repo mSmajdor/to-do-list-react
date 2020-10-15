c54c8c98ee772d1aa571efa0d4db56ff5c76326e
Mariusz Smajdor <smaidorr@gmail.com>
2020-10-15 17:40:15 +0200

    addNewTask interaction

--- src/App.js
+++ src/App.js
@@ -42,12 +42,26 @@ function App() {
     );
   };
 
+  const addNewTask = (content) => {
+    setTasks((tasks) => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ]);
+  };
+
   return (
     <Container>
       <header>
         <Header />
       </header>
-      <Section title="Dodaj nowe zadanie" body={<Form />} />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form addNewTask={addNewTask} />}
+      />
       <Section
         title="Lista zadań"
         extraHeaderContent={
